a6aa2e5437b6b8edfcb115222aa43f2e996a69a1
menacedjava <menacedjava@users.noreply.github.com>
2025-10-13 00:52:50 +0500

    feat: add character system

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
 Команда AT признана нерекомендуемой. Используйте вместо нее schtasks.exe.
 
 Такой запрос не поддерживается.
+// Character system added
+public class Character {
+    String name;
+    int level;
+}
